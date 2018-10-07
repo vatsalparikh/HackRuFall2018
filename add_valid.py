52a29231fad56ebd55f9117f50c6a5d1b70b3bf3
vatsalparikh <vatsalparikh26@outlook.com>
2018-10-07 10:45:06 -0400

    added comments and stub for similarity_matching function

--- add_valid.py
+++ add_valid.py
@@ -1,5 +1,7 @@
 import csv
+import difflib
 
+# input address provided by address standardization tool
 standard_add = ['1775',"RUSKIN","COURT","MIAMI","FLORIDA",'33180','1732']
 
 STREET_NUM = 0
@@ -10,6 +12,7 @@ STATE = 4
 ZIP = 5
 ZIP4 = 6
 
+# validating address is real or fake by comparing the standardized address with main database. Assuming city and state are valid and correct
 def validate_address(standard_add):
     with open('/home/vatsal/Downloads/HackRU/MOCK_DATA.csv') as csv_file:
         file = csv.reader(csv_file, delimiter=',')
@@ -27,6 +30,20 @@ def validate_address(standard_add):
                     line += 1
 
 print(validate_address(standard_add))
+
+# similarity match compares the validated address (input) from above for each address field
+# and returns the most probable address that's closest to a valid and real address
+
+def similarity_match(pri_add, predirect, street, suffix, postdirect, sec_add_identify, sec_add, city, state, zip, zip4):
+
+# start by comparing each input field in input address with corresponding field in a list of matched addresses
+# Prioritize by state, zip, city, street name, etc.
+# store each field comparison value for every similarity comparison.
+    difflib.SequenceMatcher(None, pri_add[0][1], street[0][1]).ratio()
+# the field with highest value according to priority is stored and the others are discarded.
+# we continue by comparing the next field, and storing maximum priorities.
+# once all the fields are compared, the max value with similarity matching is declared as the most probable real address.
+
 
 
 
