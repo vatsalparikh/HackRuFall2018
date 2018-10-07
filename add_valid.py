2dd79e40c7f9b767abb0365e061be5339207aa82
vatsalparikh <vatsalparikh26@outlook.com>
2018-10-07 09:52:38 -0400

    debugged add_valid.py

--- add_valid.py
+++ add_valid.py
@@ -1,6 +1,6 @@
 import csv
 
-standard_add = [1175,"RUSKIN","COURT","MIAMI","FLORIDA",33180,1732]
+standard_add = ['1775',"RUSKIN","COURT","MIAMI","FLORIDA",'33180','1732']
 
 STREET_NUM = 0
 STREET_NAME = 1
@@ -9,13 +9,6 @@ CITY = 3
 STATE = 4
 ZIP = 5
 ZIP4 = 6
-
-max_val = None
-
-with open('/home/vatsal/Downloads/HackRU/MOCK_DATA.csv') as f:
-    reader = csv.reader(f)
-    next(reader)
-    max_val = max(int(column[STREET_NUM].replace(',', '')) for column in reader)
 
 def validate_address(standard_add):
     with open('/home/vatsal/Downloads/HackRU/MOCK_DATA.csv') as csv_file:
@@ -26,7 +19,7 @@ def validate_address(standard_add):
                 line += 1
             else:
                 if standard_add[STATE] == row[STATE] and standard_add[CITY] == row[CITY]:
-                    if (standard_add[STREET_NUM] <= max_val) and (standard_add[STREET_NAME] == row[STREET_NAME]) and (standard_add[ZIP] == row[ZIP]):
+                    if (standard_add[STREET_NUM] == row[STREET_NUM]) and (standard_add[STREET_NAME] == row[STREET_NAME]) and (standard_add[ZIP] == row[ZIP]):
                         return True
                     else:
                         continue
